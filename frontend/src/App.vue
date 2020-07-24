152a9ec5942c5de0399f45dab1b3247b64053aad
wjun7979 <wjun7979@gmail.com>
2020-07-24 10:26:56 +0800

    编写前端首页面

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+        <el-header>Header</el-header>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueResource from "vue-resource";
+
+Vue.use(ElementUI);
+Vue.use(VueResource);
 
 Vue.config.productionTip = false
 
